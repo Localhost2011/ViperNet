0f791b399e68096708c8a878b933d579da9439ba
Localhost2011 <120727229+Localhost2011@users.noreply.github.com>
2023-02-16 17:20:34 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-async function signInWithEmail() {
-  const { data, error } = await supabase.auth.signInWithOtp({
-    email: (username),
-    options: {
-      emailRedirectTo: 'https://viper-net.vercel.app/',
-    },
-  })
-}
